ed81e8ee8d1daa771cf6e3e908dce5b2430af46b
Mark <deviousmalcontent@gmail.com>
2024-12-02 15:29:23 +1100

    Code clean-up.

--- day01/day01-part2.cpp
+++ day01/day01-part2.cpp
@@ -11,6 +11,26 @@
 #include <map>
 #include <iterator>
 
+/*
+Declare x2 arrays to store each number block in,
+
+Read in the text file and pass it using a delimiter, in this case the delimiter is 3 spaces,
+split each line of text up and add them to each individual array,
+finding the first block is pretty easy it's just a substring and we stop at the first index of the delimiter,
+the second block we need to take the first blocks length and the delimiters length add them together,
+that's the offset, and it ends when the line breaks, (there is probably a bug calling str.find(delimiter) at the end of the second block, lol).
+
+Load these values up into each array while we parse the text file, then sort the array blocks low to high.
+
+Because our focus is now on the table on the right, let's create a map of counts and count instances of each value in this block, and store it in its own array (or Map). - maps are just 2D arrays.
+
+Finally in the Match and do Calc section of the code let's iterate over every element in the first block array, 
+check if that element exists in the second block array and then get the number of times it occurs in the second block array, 
+this is done by accessing the counts array from earlier an using the value as the key or index, 
+this is where we do the multiplication an append it to the sum, 
+finally we print the sum and there's our answer.
+*/
+
 template<typename C, typename T>
 bool contains(C&& c, T e) {
     return std::find(std::begin(c), std::end(c), e) != std::end(c);
@@ -20,14 +40,15 @@ int main()
 {
     std::vector<int> firstBlockArray, secondBlockArray;
 
-    //std::ifstream file("input-test");
-    std::ifstream file("input");
+    std::ifstream file("input-test");
+    //std::ifstream file("input");
     std::string str;
     while (std::getline(file, str))
     {
         std::string delimiter = "   ";
         std::string firstBlock = str.substr(0, str.find(delimiter));
-        std::string secondBlock = str.substr(firstBlock.length() + delimiter.length(), str.find(delimiter));
+        //std::string secondBlock = str.substr(firstBlock.length() + delimiter.length(), str.find(delimiter));
+        std::string secondBlock = str.substr(firstBlock.length() + delimiter.length(), str.length());
 
         firstBlockArray.push_back(std::stoi(firstBlock));
         secondBlockArray.push_back(std::stoi(secondBlock));
@@ -66,7 +87,6 @@ int main()
         }
         else {
             std::cout << "The number " << firstBlockArray[i] << " was NOT found in the Second Block Array." << std::endl;
-
             //sum = sum + firstBlockArray[i];
         }
     }
